cadcc9f65e929e23264f03a0fe6a883850e1cae6
leonleon123 <lele.slav@gmail.com>
2021-12-02 12:29:27 +0100

    🔧 refactored day 1 using numpy

--- 01/main.py
+++ 01/main.py
@@ -1,5 +1,6 @@
+from numpy import array, diff, sum
+from numpy.lib.stride_tricks import sliding_window_view as swv
+
 with open('input.txt') as file:
-    nums = [int(x) for x in file.readlines()]
-diff = lambda arr: [arr[i+1] - arr[i] for i in range(len(arr)-1)]
-sums = [sum(nums[i:i+3]) for i in range(len(nums)-2)]
-print(*[sum([x > 0 for x in diff(arr)]) for arr in [nums, sums]], sep='\n')
+    nums = array([int(x) for x in file.readlines()])
+print(*[sum(diff(arr) > 0) for arr in [nums, sum(swv(nums, 3), axis=1)]], sep='\n')
